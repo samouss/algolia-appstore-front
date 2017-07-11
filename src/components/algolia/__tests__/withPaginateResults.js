b965c783f460d510d4410ed76ba6a379ce2eae25
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-07-11 10:11:47 +0200

    feat(algolia): add withPaginateResults HoC

---
+++ src/components/algolia/__tests__/withPaginateResults.js
@@ -0,0 +1,234 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { shallow } from 'enzyme';
+import { createMockAlgoliaClient, createMockAlgoliaHelper } from 'test/algolia';
+import withPaginateResults from '../withPaginateResults';
+
+describe('algolia', () => {
+  describe('<Provider />', () => {
+    const createContext = () => ({
+      algoliaHelper: createMockAlgoliaHelper(),
+      algoliaClient: createMockAlgoliaClient(),
+    });
+
+    const Component = ({ children, className }) => (
+      <div className={className}>{children}</div>
+    );
+
+    Component.propTypes = {
+      className: PropTypes.string.isRequired,
+      children: PropTypes.node.isRequired,
+    };
+
+    Component.defaultProps = {
+      className: '',
+    };
+
+    it('expect to render', () => {
+      const context = createContext();
+
+      const props = {
+        className: 'sample-class-name',
+      };
+
+      const ApplyComponent = withPaginateResults()(Component);
+
+      const component = shallow(
+        <ApplyComponent
+          {...props}
+        >
+          <div>Content</div>
+        </ApplyComponent>,
+        { context },
+      );
+
+      expect(component).toMatchSnapshot();
+    });
+
+    describe('componentDidMount', () => {
+      it('expect to call setQueryParameter with default parameter', () => {
+        const context = createContext();
+        const ApplyComponent = withPaginateResults()(Component);
+
+        const component = shallow(
+          <ApplyComponent>
+            <div>Content</div>
+          </ApplyComponent>,
+          { context },
+        );
+
+        component.instance().componentDidMount();
+
+        expect(context.algoliaHelper.setQueryParameter).toHaveBeenCalledWith(
+          'hitsPerPage',
+          25,
+        );
+      });
+
+      it('expect to call setQueryParameter with given parameter', () => {
+        const context = createContext();
+        const ApplyComponent = withPaginateResults({
+          hitsPerPage: 50,
+        })(Component);
+
+        const component = shallow(
+          <ApplyComponent>
+            <div>Content</div>
+          </ApplyComponent>,
+          { context },
+        );
+
+        component.instance().componentDidMount();
+
+        expect(context.algoliaHelper.setQueryParameter).toHaveBeenCalledWith(
+          'hitsPerPage',
+          50,
+        );
+      });
+
+      it('expect to subscribe to result event', () => {
+        const context = createContext();
+        const ApplyComponent = withPaginateResults()(Component);
+
+        const component = shallow(
+          <ApplyComponent>
+            <div>Content</div>
+          </ApplyComponent>,
+          { context },
+        );
+
+        component.instance().componentDidMount();
+
+        expect(context.algoliaHelper.on).toHaveBeenCalledWith(
+          'result',
+          component.instance().updateState,
+        );
+      });
+    });
+
+    describe('componentWillUnmount', () => {
+      it('expect to unsubscribe to result event', () => {
+        const context = createContext();
+        const ApplyComponent = withPaginateResults()(Component);
+
+        const component = shallow(
+          <ApplyComponent>
+            <div>Content</div>
+          </ApplyComponent>,
+          { context },
+        );
+
+        component.instance().componentWillUnmount();
+
+        expect(context.algoliaHelper.removeListener).toHaveBeenCalledWith(
+          'result',
+          component.instance().updateState,
+        );
+      });
+    });
+
+    describe('onNextPage', () => {
+      it('expect to call nextPage and search', () => {
+        const context = createContext();
+        const ApplyComponent = withPaginateResults()(Component);
+
+        const component = shallow(
+          <ApplyComponent>
+            <div>Content</div>
+          </ApplyComponent>,
+          { context },
+        );
+
+        component.instance().onNextPage();
+
+        expect(component.state().isEndReached).toBe(false);
+        expect(context.algoliaHelper.nextPage).toHaveBeenCalled();
+        expect(context.algoliaHelper.search).toHaveBeenCalled();
+      });
+
+      it('expect to call nextPage on last page and search', () => {
+        const context = createContext();
+        const ApplyComponent = withPaginateResults()(Component);
+
+        const component = shallow(
+          <ApplyComponent>
+            <div>Content</div>
+          </ApplyComponent>,
+          { context },
+        );
+
+        component.setState({
+          nbPages: 5,
+          page: 4,
+        });
+
+        component.instance().onNextPage();
+
+        expect(component.state().isEndReached).toBe(true);
+        expect(context.algoliaHelper.nextPage).toHaveBeenCalled();
+        expect(context.algoliaHelper.search).toHaveBeenCalled();
+      });
+
+      it('expect to not call nextPage and search', () => {
+        const context = createContext();
+        const ApplyComponent = withPaginateResults()(Component);
+
+        const component = shallow(
+          <ApplyComponent>
+            <div>Content</div>
+          </ApplyComponent>,
+          { context },
+        );
+
+        component.setState({
+          isEndReached: true,
+        });
+
+        component.instance().onNextPage();
+
+        expect(context.algoliaHelper.nextPage).not.toHaveBeenCalled();
+        expect(context.algoliaHelper.search).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('updateState', () => {
+      it('expect to update the state from aloglia result', () => {
+        const context = createContext();
+        const ApplyComponent = withPaginateResults()(Component);
+
+        const component = shallow(
+          <ApplyComponent>
+            <div>Content</div>
+          </ApplyComponent>,
+          { context },
+        );
+
+        const content = {
+          hits: [
+            { objectID: '123' },
+            { objectID: '456' },
+          ],
+          nbHits: 1250,
+          nbPages: 20,
+          page: 5,
+        };
+
+        const expectation = {
+          hits: [
+            { objectID: '123' },
+            { objectID: '456' },
+          ],
+          nbHits: 1250,
+          nbPages: 20,
+          page: 6,
+          isEndReached: false,
+          isInitialLoad: false,
+        };
+
+        component.instance().updateState(content);
+
+        expect(component.state()).toEqual(expectation);
+      });
+    });
+  });
+});
---
+++ src/components/algolia/withPaginateResults.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import { getDisplayName } from 'core/utils';
+import { ContextTypes } from './Provider';
+
+const withPaginateResults = ({
+  hitsPerPage = 25,
+} = {}) => WrappedComponent => {
+  class WithPaginateResults extends Component {
+
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        hits: [],
+        nbHits: 0,
+        nbPages: 0,
+        page: 1,
+        isEndReached: false,
+        isInitialLoad: true,
+      };
+
+      this.onNextPage = this.onNextPage.bind(this);
+      this.updateState = this.updateState.bind(this);
+    }
+
+    componentDidMount() {
+      const { algoliaHelper } = this.context;
+
+      algoliaHelper.setQueryParameter(
+        'hitsPerPage',
+        hitsPerPage,
+      );
+
+      algoliaHelper.on('result', this.updateState);
+    }
+
+    componentWillUnmount() {
+      this.context.algoliaHelper.removeListener('result', this.updateState);
+    }
+
+    onNextPage() {
+      const { page, nbPages, isEndReached } = this.state;
+
+      if (isEndReached) {
+        return;
+      }
+
+      const newPage = page + 1;
+      const isLastPage = newPage === nbPages;
+
+      this.setState(() => ({
+        isEndReached: isLastPage,
+      }));
+
+      this.context.algoliaHelper
+        .nextPage()
+        .search();
+    }
+
+    updateState(content) {
+      this.setState(prevState => ({
+        hits: prevState.hits.concat(content.hits),
+        nbHits: content.nbHits,
+        nbPages: content.nbPages,
+        page: content.page + 1,
+        isInitialLoad: false,
+      }));
+    }
+
+    render() {
+      return (
+        <WrappedComponent
+          {...this.props}
+          {...this.state}
+          onNextPage={this.onNextPage}
+        />
+      );
+    }
+
+  }
+
+  WithPaginateResults.displayName = getDisplayName(
+    WrappedComponent,
+    'withPaginateResults',
+  );
+
+  WithPaginateResults.contextTypes = ContextTypes;
+
+  return WithPaginateResults;
+};
+
+export default withPaginateResults;
