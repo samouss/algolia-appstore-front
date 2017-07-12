6f83942876ac0b888ffa90401c4895dafa4c4a6e
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-07-12 12:58:41 +0200

    feat(TextInput): add clear button

--- src/components/TextInput/__tests__/index.js
+++ src/components/TextInput/__tests__/index.js
@@ -23,7 +23,37 @@ describe('<TextInput />', () => {
     expect(component).toMatchSnapshot();
   });
 
-  it('expect to call onChange', () => {
+  it('expect to render without clear button when value is empty', () => {
+    const props = {
+      ...defaultProps,
+      value: '',
+    };
+
+    const component = shallow(
+      <TextInput
+        {...props}
+      />,
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('expect to render without clear button when disabled', () => {
+    const props = {
+      ...defaultProps,
+      enableClearButton: false,
+    };
+
+    const component = shallow(
+      <TextInput
+        {...props}
+      />,
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('expect to call onChange on input change', () => {
     const props = {
       ...defaultProps,
       onChange: jest.fn(),
@@ -40,5 +70,28 @@ describe('<TextInput />', () => {
       .simulate('change');
 
     expect(props.onChange).toHaveBeenCalled();
+  });
+
+  it('expect to call onChange on input clear', () => {
+    const props = {
+      ...defaultProps,
+      onChange: jest.fn(),
+    };
+
+    const component = shallow(
+      <TextInput
+        {...props}
+      />,
+    );
+
+    component
+      .find('i')
+      .simulate('click');
+
+    expect(props.onChange).toHaveBeenCalledWith({
+      currentTarget: {
+        value: '',
+      },
+    });
   });
 });
