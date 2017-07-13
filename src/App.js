3bf6870c8875c25f6b67f8a726a95c22870df6c5
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-07-13 11:38:41 +0200

    feat(App): use new Layout

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import React from 'react';
 import { Provider } from 'components/Algolia';
-import Layout from 'components/Layout';
-import Filters from 'components/Filters';
-import Search from 'containers/Search';
+import { Container, Filters, Advanced, Column, Row } from 'components/Layout';
+import SearchFilter from 'containers/SearchFilter';
+import CategoryFilter from 'containers/CategoryFilter';
+import RatingSort from 'containers/RatingSort';
 import InfiniteAppList from 'containers/InfiniteAppList';
 
 const appId = 'RTMJH2ID8J';
 const apiKey = 'f8dac63a6dc38d8bd5893dad8969d819';
-const indexName = 'apps';
+const indexName = 'apps_rating_desc';
+const options = {
+  disjunctiveFacets: ['category'],
+};
 
 const App = () => (
   <Provider
     appId={appId}
     apiKey={apiKey}
     indexName={indexName}
+    options={options}
   >
-    <Layout>
+    <Container>
       <Filters>
-        <Search />
+        <SearchFilter />
+
+        <Advanced>
+          <Column>
+            <Row>
+              <CategoryFilter />
+            </Row>
+          </Column>
+
+          <Column>
+            <Row>
+              <RatingSort />
+            </Row>
+          </Column>
+        </Advanced>
       </Filters>
 
       <InfiniteAppList />
-    </Layout>
+    </Container>
   </Provider>
 );
 
