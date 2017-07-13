49568cf9a8b423b0fafe54aa8eb74ce6fc927959
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-07-13 12:52:19 +0200

    feat(server): setup

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import compression from 'compression';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(compression());
+app.use(express.static(__dirname));
+
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Server is running on', port);
+});
