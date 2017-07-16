43c7b788b951bbf2b4ede481f22f54fd6a10ec3e
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-07-16 14:14:51 +0200

    feat(test): add clearRefinements to mock helper

--- src/test/algolia.js
+++ src/test/algolia.js
@@ -1,18 +1,22 @@
 export const createMockAlgoliaClient = () => ({});
 
 export const createMockAlgoliaHelper = () => ({
-  // Configuration
+  // Index
   getIndex: jest.fn(() => 'index_name'),
   setIndex: jest.fn().mockReturnThis(),
+
+  // Query
   setQuery: jest.fn().mockReturnThis(),
   setQueryParameter: jest.fn().mockReturnThis(),
 
-  // Faceting
+  // Refinements
   addDisjunctiveFacetRefinement: jest.fn().mockReturnThis(),
   removeDisjunctiveFacetRefinement: jest.fn().mockReturnThis(),
 
   addNumericRefinement: jest.fn().mockReturnThis(),
   removeNumericRefinement: jest.fn().mockReturnThis(),
+
+  clearRefinements: jest.fn().mockReturnThis(),
 
   // Pagination
   nextPage: jest.fn().mockReturnThis(),
