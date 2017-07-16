0f77aaacf406af7319802a67896c4199c3fb6426
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-07-16 11:52:32 +0200

    feat(polyfills): add fetch and promise polyfill

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,3 +1,8 @@
-// Only import polyfill you need
+// Genric polyfill
+import 'whatwg-fetch';
 
+// Import core-js modules
+import 'core-js/es6/promise';
+
+// Import core-js functions
 import 'core-js/fn/array/includes';
